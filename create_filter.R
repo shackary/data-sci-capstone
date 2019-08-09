1d2ccd0f00abb52b25b2a1bdf5561cb2f7dd3852
Zach Sharrow <sharrowz@gmail.com>
2019-08-09 17:00:03 -0400

    now writes the filtered lines to a file

--- create_filter.R
+++ create_filter.R
@@ -1,5 +1,13 @@
 ## Generates the profanity list to be used in filtering
 
+## The main function removes lines containing profanity from a character vector
+censorize <- function(file, profanity){
+    bad_lines <- sapply(profanity, function(x) grepl(x, file, ignore.case = T))
+    bad_lines <- apply(bad_lines, 1, any)
+    print(paste("removed", as.character(sum(bad_lines)), "entries"))
+    file[!bad_lines]
+}
+
 ## Get list from the source
 list <- url("https://raw.githubusercontent.com/RobertJGabriel/Google-profanity-words/master/list.txt", open="rt")
 bad_words <- readLines(list)
@@ -12,9 +20,21 @@ bad_words <- c(bad_words, "bullshit")
 bad_words <- paste(bad_words, collapse = "|", sep = "")
 bad_words <- paste0("\\b(", bad_words, ")\\b")
 
-## Write to file
-writeLines(bad_words, "./final/en_US/filter.txt")
+## Read in the samples
+twitter <- readLines("./final/en_US/twitter_sample.txt", encoding = "UTF-8")
+blogs <- readLines("./final/en_US/blog_sample.txt", encoding = "UTF-8")
+news <- readLines("./final/en_US/news_sample.txt", encoding = "UTF-8")
+
+## Filter them
+twitter <- censorize(twitter, bad_words)
+news <- censorize(news, bad_words)
+blogs <- censorize(blogs, bad_words)
+
+## Write the results to a new file
+writeLines(twitter, "./final/en_US/twitter_filtered.txt", useBytes = T)
+writeLines(blogs, "./final/en_US/blog_filtered.txt", useBytes = T)
+writeLines(news, "./final/en_US/news_filtered.txt", useBytes = T)
 
 ## Clean up
 closeAllConnections()
-rm(list, bad_words)
+rm(list, bad_words, twitter, news, blogs)
