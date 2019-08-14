9162b20544a2635158a3dd7e147549c2d40b0d8b
Zach Sharrow <sharrowz@gmail.com>
2019-08-14 16:59:29 -0400

    slow progress

--- script.R
+++ script.R
@@ -2,10 +2,18 @@ library(tidytext)
 library(quanteda)
 
 ## Read in data
-twitter <<- readLines("./final/en_US/twitter_filtered.txt", encoding = "UTF-8")
-blogs <<- readLines("./final/en_US/blog_filtered.txt", encoding = "UTF-8")
-newses <<- readLines("./final/en_US/news_filtered.txt", encoding = "UTF-8")
+twitter <- readLines("./final/en_US/twitter_filtered.txt", encoding = "UTF-8")
+blogs <- readLines("./final/en_US/blog_filtered.txt", encoding = "UTF-8")
+news <- readLines("./final/en_US/news_filtered.txt", encoding = "UTF-8")
 
+
+## Concatenate them into the full data set, clean up
+full_corp <- c(twitter, blogs, news)
+rm(twitter, blogs, news)
+
+## Generate quanteda corpus and document feature matrix
+corpus <- corpus(full_corp)
+features <- dfm(corpus)
 
 
 
